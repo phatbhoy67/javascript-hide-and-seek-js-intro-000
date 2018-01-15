5dfb5733312860fea9d1737920e1a96779f341ab
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-15 10:12:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let rankList = document.querySelectorAll('ul.ranked-list li');
 
   for(let i = 0; i < rankList.length; i++){
   let currVal = parseInt(rankList[i].innerHTML);
-  let currVal += n;
+  currVal += n;
   rankList[i].innerHTML = currVal;
   }
 }
