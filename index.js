42b4a2a67d40f269b246ecf817b562bfb89d74db
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-15 10:11:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild(){}
+
+function increaseRankBy(n){
+let rankList = document.querySelectorAll('ul.ranked-list li');
+
+  for(let i = 0; i < rankList.length; i++){
+  currVal = parseInt(rankList[i].innerHTML);
+  currVal += n;
+  rankList[i].innerHTML = currVal;
+  }
+}
